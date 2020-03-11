b4506381e864d958e1c5c99428fe73629f01cc24
lPraxy <32679901+lPraxy@users.noreply.github.com>
2020-03-11 13:01:56 -0400

    ajustement classe grille 2

--- Demineur/Grille.cs
+++ Demineur/Grille.cs
@@ -6,13 +6,13 @@ namespace Demineur
 {
     public class Grille
     {
-        Case[][] champs;
+        Case[,] champs;
         short nbBombeGrille;
 
         public Grille(short colonne, short ligne, short nbBombe)
         {
             nbBombeGrille = nbBombe;
-            champs = new Case[colonne][ligne];
+            champs = new Case[colonne, ligne];
 
             for(int i = 0; i < colonne; i++)
             {
@@ -28,15 +28,16 @@ namespace Demineur
 
         public void DisperserBombes(short colonne, short ligne)
         {
-            random randomI = new random();
-            random randomJ = new random();
+            Random random = new Random();
 
-            for(int k = nbBombeGrille; k > 0; k--)
+            Case destination;
+
+            for (int k = nbBombeGrille; k > 0; k--)
             {
-                destination = champs[randomI % colonne][randomJ % ligne];
+                destination = champs[random.Next(0, colonne), random.Next(0, ligne)];
 
                 while(destination.Bombe)
-                    destination = champs[randomI % colonne][randomJ % ligne];
+                    destination = champs[random.Next(0, colonne), random.Next(0, ligne)];
 
                 destination.Bombe = true;
             }
@@ -48,31 +49,31 @@ namespace Demineur
             {
                 for (int j = 0; j < ligne; j++)
                 {
-                    destination = champs[i][j];
+                    Case destination = champs[i, j];
                     
                     if ((i - 1 > 0) && (j - 1 > 0))//NW
-                        destination[0] = champs[i - 1][j - 1];
+                        destination[0] = champs[i - 1, j - 1];
 
                     if (j - 1 > 0)//N
-                        destination[1] = champs[i][j - 1];
+                        destination[1] = champs[i, j - 1];
 
                     if ((i + 1 < colonne) && (j - 1 > 0))//NE
-                        destination[2] = champs[i + 1][j - 1];
+                        destination[2] = champs[i + 1, j - 1];
 
                     if (i - 1 > 0)//W
-                        destination[3] = champs[i - 1][j];
+                        destination[3] = champs[i - 1, j];
 
                     if (i + 1 < colonne)//E
-                        destination[4] = champs[i + 1][j];
+                        destination[4] = champs[i + 1, j];
 
                     if ((i - 1 > 0) && (j + 1 < ligne))//SW
-                        destination[5] = champs[i - 1][j + 1];
+                        destination[5] = champs[i - 1, j + 1];
 
                     if (j + 1 < ligne)//S
-                        destination[6] = champs[i][j + 1];
+                        destination[6] = champs[i, j + 1];
 
                     if ((i + 1 < colonne) && (j + 1 < ligne))//SE
-                        destination[7] = champs[i + 1][j + 1];
+                        destination[7] = champs[i + 1, j + 1];
                 }
             }
         }
