8acdc20b008978dd05d4cc0e0f83be57722f34a7
Marc-André Larocque Blais <marcandrelarocqueblais@gmail.com>
2020-03-10 20:15:06 -0400

    Code cleanup de l'interface, ajout du support de la touche entrer

--- Demineur/InterfaceUsager.cs
+++ Demineur/InterfaceUsager.cs
@@ -8,138 +8,140 @@ namespace Demineur
     public static class InterfaceUsager //static pour test
     {
         static string ModeDeJeuActif = "Contrôle par flèche";
-        public static void DessinerTitreJeu(int col)
+        static string marge = "    "; // Unité d'espacement.
+        static void DessinerTitreJeu(int col)
         {
             string titre = "Démineur 2020, année du JUGEMENT dernier.";
-            int posTitre = (((16 * 4) / 2) - (titre.Length / 2)) + 4; // colonnes harcoder 16
-            DessinerInstructions(col * 4 + 8); // colonnes harcoder 20
+            int posTitre = (((col * 4) / 2) - (titre.Length / 2)) + 4; // Centrage automatique
+            DessinerInstructions(col * 4 + 8);
             Console.SetCursorPosition(posTitre, 0);
             Console.WriteLine(titre + "\n");
         }
 
-        public static void DessinerInstructions(int offset)
+        static void DessinerInstructions(int offset)
         {           
             Console.SetCursorPosition(offset, 4);
             Console.Write("Appuyez sur f pour utiliser les flèches.");
             Console.SetCursorPosition(offset, 5);
             Console.Write("Appuyez sur c pour entrer des coordonnées au clavier.");
-            Console.SetCursorPosition(offset, 9);
+            Console.SetCursorPosition(offset, 6);
+            Console.Write("Appuyez sur Entrer pour confirmer votre sélection.");
+            Console.SetCursorPosition(offset, 10);
             Console.Write("Mode de jeu actif : " + ModeDeJeuActif);
-
         }
 
 
-        public static void DessinerPiedDePage(int iCol, string nomJoueur, int nbCouts)
+        static void DessinerStats(int nColonne, string nomJoueur, int nbCouts)
         { //Dessine le timer, le nom du joueur et le petit bonhomme
 
-            Console.Write("    ");
-            for (int y = 0; y < iCol; y++) //Dessine ligne du haut
-                Console.Write(" ___");
+            DessinerLigneDuHaut(nColonne);
 
-            Console.Write("\n");
             for (int x = 0; x < 5; x++) // Dessine ligne 
             {
-                Console.Write("    |");
+                Console.Write(marge + "|");
                 if (x == 4)
-                    for (int y = 0; y < iCol; y++)
+                    for (int y = 0; y < nColonne; y++)
                         Console.Write("___ ");
                 else if (x == 2)    //Dessine ligne 3 de l'entete (Nom, yeux, nbCout)
                 {
-                    for (int y = 0; y < iCol; y++)
+                    for (int y = 0; y < nColonne; y++)
                         if (y == 1)
                             Console.Write(nomJoueur);
-                        else if (y == iCol - 2)
+                        else if (y == nColonne - 2)
                             Console.Write(nbCouts); // doit être formatté pour prendre un espace fixe.
-                        else if (y == (iCol / 2) - 1)
+                        else if (y == (nColonne / 2) - 1)
                             Console.Write("  :D   ");
                         else
-                            Console.Write("    ");
+                            Console.Write(marge);
                 }
                 else
-                    for (int y = 0; y < iCol; y++) //Dessine ligne vide
-                        Console.Write("    ");
+                    DessinerRangeVide(nColonne);
 
                 Console.Write("\b|");
                 Console.Write("\n");
             }
         }
 
-        public static void DessinerGrille(int col, int lig, string grille, short posX, short posY)
+        public static void DessinerGrille(int nColonne, int nRange, string grille, short posX, short posY)
         {
-
+            Console.SetWindowSize(nColonne*3 + 80,40);
             Console.Clear();
-            Console.SetWindowSize(col*3 + 80,40);
-            DessinerTitreJeu(col);
+            
+            DessinerTitreJeu(nColonne);
+            DessinerChiffreColonne(nColonne);
+            DessinerLigneDuHaut(nColonne);
 
-            for (int x = 0; x < lig; x++)
+            for (int x = 0; x < nRange; x++)
             {
-                if (x == 0)
-                {
-                    Console.Write("   "); // left offset for top numbers
-                    for (int y = 0; y < col; y++)
-                    {
-                        if (y < 10) // spacing between top numbers
-                            Console.Write(" ");
-                        Console.Write(" " + (y + 1) + " ");
-                    }
-                    Console.Write("\n");
-
-                    for (int y = 0; y < col; y++)
-                    {
-                        if (y == 0)
-                            Console.Write("    ");
-                        Console.Write(" ___");
-                    }
-                    Console.Write("\n");
-                }
-
-                for (int y = 0; y < col; y++)
-                {
-                    if (y == 0)
-                        Console.Write("    |   |");
-                    else
-                        Console.Write("   |");
-                }
-                Console.Write("\n");
-
-                for (int y = 0; y < col; y++)
-                {
-                    if (y == 0)
-                    {
-                        if (x + 1 < 10)
-                            Console.Write(" " + (x + 1) + "  | ");
-                        else
-                            Console.Write(" " + (x + 1) + " | ");
-                    }
-                    Console.Write('*' + " | "); // Contenue de tableau hardcoder
-                }
-                Console.Write("\n");
-
-                for (int y = 0; y < col; y++)
-                {
-                    if (y == 0)
-                        Console.Write("    |___|");
-                    else
-                        Console.Write("___|");
-                }
-                Console.Write("\n");
+                DessinerRangeHautCase(nColonne);
+                DessinerRangeCentraleCase(nColonne, nRange);
+                DessinerRangeBasCase(nColonne, nRange);              
             }
-            DessinerPiedDePage(col, "marc", 3);
-            Console.Write("\n   Quelle case souhaitez-vous ouvrir ? >> 1 1");
-            Cout(col, lig, "ree"); // ree as string tab
+
+            DessinerStats(nColonne, "marc", 3);
+
+            Console.Write("\n"+ marge + "Quelle case souhaitez-vous ouvrir ? >> 1 1");
+            
+            Cout(nColonne, nRange, "ree"); // Recevra le toString du tableau.
         }
         
-        public static void Cout(int iCol, int iLig, string tab)
+        static void DessinerChiffreColonne(int col){
+            Console.Write(marge);
+            for (int y = 0; y < col; y++)
+            {
+                if (y < 10) // spacing between top numbers
+                    Console.Write(" ");
+                Console.Write(" " + (y + 1) + " ");
+            }
+            Console.Write("\n");
+        }
+
+        static void DessinerLigneDuHaut(int col){
+            Console.Write(marge);
+            for (int y = 0; y < col; y++)
+                Console.Write(" ___");
+            Console.Write("\n");
+        }
+
+        static void DessinerRangeHautCase(int col){
+            Console.Write("    |   |");
+            for (int y = 1; y < col; y++)
+                Console.Write("   |"); //Ajoute ligne vertical de droite pour chaque autre colonne.
+            Console.Write("\n");
+        }
+
+        static void DessinerRangeCentraleCase(int col, int range){
+
+            if (range + 1 < 10)
+                Console.Write(" " + (range + 1) + "  | "); // spacing de gauche pour les chiffres des rangés < 10
+            else
+                Console.Write(" " + (range + 1) + " | "); // spacing de gauche pour les chiffres des rangés >= 10
+            for (int y = 0; y < col; y++)
+                Console.Write('*' + " | "); // Contenue de tableau hardcoder
+            Console.Write("\n");
+        }
+
+        static void DessinerRangeBasCase(int col, int range){
+            Console.Write("    |___|");
+            for (int y = 1; y < col; y++)
+                Console.Write("___|");
+            Console.Write("\n");
+        }
+
+        static void DessinerRangeVide(int nColonne){
+            for (int y = 0; y < nColonne; y++)
+                Console.Write(marge);
+        }
+
+        public static int[] Cout(int iCol, int iLig, string tab)
         {
-            ConsoleKeyInfo arrow;
-            bool boucle = true;
-            int[] positionActuelle = new int[2] {6, 5}; // position au début de la partie
-            string choix;
+            int[] positionActuelle = new int[2] {6, 5}; // Position au début de la partie.
+            ConsoleKeyInfo arrow;                       // Info de la flèche appuyé.                     
+            int[] choix = new int[2];                               // Coordonnées choisies
 
             Console.SetCursorPosition(positionActuelle[0], positionActuelle[1]);
-            while (boucle)
+            do
             {
-
                 arrow = Console.ReadKey();
 
                 switch ((int)arrow.Key)
@@ -179,36 +181,32 @@ namespace Demineur
                         break;
                     case 13:
                         positionActuelle = new int[2] { Console.CursorLeft, Console.CursorTop };
-                        boucle = false;
                         break;
-                    case 70: // f pour controler avec fleches                      
-                     //   positionActuelle = new int[2] { Console.CursorLeft-1, Console.CursorTop };                       
+                    case 70: // f pour controler avec fleches                                          
                         Console.Write("\b");
                         ModeDeJeuActif = "Contrôle par flèche";
                         DessinerInstructions(iCol * 4 + 8);                               
                         break;
-                    case 67: // c pour coordonnées manuelles
-                     //   positionActuelle = new int[2] { Console.CursorLeft-1, Console.CursorTop };     
+                    case 67: // c pour coordonnées manuelles    
                         ModeDeJeuActif = "Entrée manuscrite   ";
                         Console.Write("\b");
                         DessinerInstructions(iCol * 4 + 8);                   
                         break;
                     case 65: // a pour aciver l'intelligence artificiel
-                        positionActuelle = new int[2] { Console.CursorLeft, Console.CursorTop };
+
                         break;
                     default:
-                        positionActuelle = new int[2] { Console.CursorLeft, Console.CursorTop };
                         Console.Clear();
                         DessinerGrille(iCol, iLig, tab, (short)Console.CursorLeft, (short)Console.CursorTop);
                         break;
                 }
-                Console.SetCursorPosition(42, iLig * 3 + 11);
+                Console.SetCursorPosition(43, iLig * 3 + 11);
                 Console.Write("      ");
-                Console.SetCursorPosition(42, iLig * 3 + 11);
-                choix = positionActuelle[0] / 4 + " " + positionActuelle[1] / 3;
-                Console.Write(choix);
+                Console.SetCursorPosition(43, iLig * 3 + 11);
+                Console.Write(positionActuelle[0] / 4 + " " + positionActuelle[1] / 3);
                 Console.SetCursorPosition(positionActuelle[0], positionActuelle[1]);
-            }
+            }while(arrow.Key != ConsoleKey.Enter);
+            return choix;
         }
 
 
